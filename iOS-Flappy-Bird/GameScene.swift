6ff8a1706e01451b42da8936983a1e895b73cb29
Nakib Momin <nbmomin14@gmail.com>
2015-09-04 00:10:29 -0400

    Added background and moving animation

--- iOS-Flappy-Bird/GameScene.swift
+++ iOS-Flappy-Bird/GameScene.swift
@@ -10,8 +10,27 @@ import SpriteKit
 
 class GameScene: SKScene {
     var bird = SKSpriteNode()
-    
+    var bg = SKSpriteNode()
     override func didMoveToView(view: SKView) {
+        
+        //bg
+        let bgTexture = SKTexture(imageNamed: "bg.png")
+ 
+        let moveBg = SKAction.moveByX(-bgTexture.size().width, y: 0, duration: 9)
+        let resetBg = SKAction.moveByX(bgTexture.size().width, y: 0, duration: 0)
+        let bgAction = SKAction.repeatActionForever(SKAction.sequence([moveBg,resetBg]))
+        
+        
+        for var i=0; i<3; i++ {
+            bg.size.height = self.frame.height
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: bgTexture.size().width/2 + bgTexture.size().width * CGFloat(i), y: frame.size.height/2.0)
+            bg.runAction(bgAction)
+            addChild(bg)
+        }
+        
+        
+        //bird
         let birdTexture = SKTexture(imageNamed: "flappy1.png")
         let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
         
@@ -22,6 +41,9 @@ class GameScene: SKScene {
         bird.position = CGPoint(x: frame.size.width/2.0, y: frame.size.height/2.0)
         bird.runAction(makeBirdFlap)
         addChild(bird)
+        
+        
+       
         
     }
     
