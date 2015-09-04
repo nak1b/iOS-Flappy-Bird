50ddfb628c4727b4ecd7e9c9b934dd3d6b81c13a
Nakib Momin <nbmomin14@gmail.com>
2015-09-03 23:26:46 -0400

    Added bird and flap animation

--- iOS-Flappy-Bird/GameScene.swift
+++ iOS-Flappy-Bird/GameScene.swift
@@ -9,8 +9,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var bird = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
         
+        let animation = SKAction.animateWithTextures([birdTexture, birdTexture2], timePerFrame: 0.1)
+        let makeBirdFlap = SKAction.repeatActionForever(animation)
+        
+        bird = SKSpriteNode(texture: birdTexture)
+        bird.position = CGPoint(x: frame.size.width/2.0, y: frame.size.height/2.0)
+        bird.runAction(makeBirdFlap)
+        addChild(bird)
         
     }
     
